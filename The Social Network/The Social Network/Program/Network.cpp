6c9a7e76003e18da0a567e30d43237e3988ef90b
Kyle Drewes <48342324+kdrewes@users.noreply.github.com>
2025-09-05 19:44:21 -0700

    Update Network.cpp

--- The Social Network/The Social Network/Program/Network.cpp
+++ The Social Network/The Social Network/Program/Network.cpp
@@ -7,7 +7,7 @@
  Date: 10/8/2022
  
  Description: This program is designed to create a social network.
- The social network is establish through the JSON file "UserList.json"
+ The social network is established through the JSON file "UserList.json"
  which contains the following attributes: name, location, picture url,
  and followers.  In this program, I have also include a list of users
  a particular profile is being followed by, as well as a list of
